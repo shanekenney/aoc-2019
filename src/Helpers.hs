234db9af3ae70f67275c74c81bc20a12d2a38510
Shane Kenney <me@shanek.org>
2019-12-15 21:56:46 +1100

    Day 09

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -3,3 +3,5 @@ module Helpers where
 parseInt :: String -> Int
 parseInt a = read a :: Int
 
+notNull :: [a] -> Bool
+notNull = not . null
--- src/IntCode.hs
+++ src/IntCode.hs
@@ -1,11 +1,13 @@
 module IntCode (execute, programVector, IntCodeMemory(..), initMemory) where
 
-import Data.Vector as V (fromList, slice, (!), (//), Vector, head, toList)
+import Data.Vector (Vector, fromList, (!), (//))
+import qualified Data.Vector as V ((++), replicate)
 import Data.Tuple.Select (sel1, sel2, sel3)
 import Data.List.Split (splitOn)
 import Helpers
 import Text.Printf
-import Debug.Trace (traceShow)
+import Data.Either
+--import Debug.Trace (traceShow)
 
 data IntCodeMemory = IntCodeMemory
   { instructionPointer :: Int
@@ -25,6 +27,7 @@ data OpCode
   | Output 
   | JumpTrue  
   | JumpFalse
+  | AdjustRelativeBase
   deriving (Show)
 
 data ParameterMode
@@ -57,38 +60,54 @@ toOpCode 5 = JumpTrue
 toOpCode 6 = JumpFalse
 toOpCode 7 = LessThan
 toOpCode 8 = Equals
+toOpCode 9 = AdjustRelativeBase
 toOpCode 99 = Halt
+toOpCode n = error $ show n ++ " is not a valid opcode."
 
 toParameterMode :: Int -> ParameterMode
 toParameterMode 0 = Position
 toParameterMode 1 = Immediate
 toParameterMode 2 = Relative
+toParameterMode n = error $ show n  ++ " is not a valid parameter mode."
 
 parseOperation :: Int -> Operation
-parseOperation code = Operation 
+parseOperation opcode = Operation 
   { code = toOpCode $ intCode
   , paramMode = (param1, param2, param3)
   }
   where
-    opCodeStr = printf "%05d" code
+    opCodeStr = printf "%05d" opcode
     intCode = parseInt $ drop 3 $ opCodeStr
     parseParameterMode = toParameterMode . parseInt . (:[]) . (!!) opCodeStr
     param1 = parseParameterMode 2
     param2 = parseParameterMode 1
     param3 = parseParameterMode 0
 
-readParam :: IntCodeMemory -> (ParameterMode, ParameterMode, ParameterMode) -> ParameterType -> ParameterPosition -> Int
-readParam memory modes paramType position = 
-  case mode of
-    Position -> case paramType of 
-                  Address -> paramValue
-                  Value -> vect ! paramValue
-    Immediate -> paramValue
-    Relative -> case paramType of 
-                  Address -> paramValue
-                  Value -> vect ! ((relativeBase memory) + paramValue)
+checkBounds :: Vector Int -> Int -> Int -> Either Int Int
+checkBounds vect index right = if length vect > index then Right right else Left index
+
+resize :: IntCodeMemory -> [Either Int Int] -> Either IntCodeMemory [Int]
+resize memory eithers =
+  if notNull outOfBounds then Left memory { vector = resizedVect } else Right (rights eithers)
   where
     vect = vector memory
+    outOfBounds = lefts eithers
+    size = (maximum outOfBounds) - (length vect) + 1
+    resizedVect = vect V.++ (V.replicate size 0)
+
+readParam :: IntCodeMemory -> (ParameterMode, ParameterMode, ParameterMode) -> ParameterType -> ParameterPosition -> Either Int Int
+readParam memory modes paramType position = 
+  case mode of
+    Immediate -> Right paramValue
+    Position -> case paramType of 
+                  Address -> check paramValue paramValue
+                  Value -> check paramValue (vect ! paramValue)
+    Relative -> case paramType of 
+                  Address -> check ((relativeBase memory) + paramValue) ((relativeBase memory) + paramValue)
+                  Value -> check ((relativeBase memory) + paramValue) (vect ! ((relativeBase memory) + paramValue))
+  where
+    vect = vector memory
+    check = checkBounds vect
     paramValue = vect ! ((instructionPointer memory) + offset)
     (mode, offset) = case position of
                        First -> (sel1 modes, 1)
@@ -96,110 +115,145 @@ readParam memory modes paramType position =
                        Third -> (sel3 modes, 3)
 
 perform :: Operation -> IntCodeMemory -> IntCodeMemory
-perform (Operation Add paramMode) memory = memory 
-  { vector = (vector memory) // [(writeAddress, value)]
-  , instructionPointer = (instructionPointer memory) + 4
-  }
+perform (Operation Add paramMode') memory =
+  case resize memory [x, y, writeAddress] of
+    Left resizedMemory -> resizedMemory
+    Right [x', y', writeAddress'] -> memory 
+      { vector = (vector memory) // [(writeAddress', (x' + y'))]
+      , instructionPointer = (instructionPointer memory) + 4
+      }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     x = readParam' Value First
     y = readParam' Value Second
     writeAddress = readParam' Address Third
-    value = x + y
 
-perform (Operation Multiply paramMode) memory = memory 
-  { vector = (vector memory) // [(writeAddress, value)]
-  , instructionPointer = (instructionPointer memory) + 4
-  }
+perform (Operation Multiply paramMode') memory =
+  case resize memory [x, y, writeAddress] of
+    Left resizedMemory -> resizedMemory
+    Right [x', y', writeAddress'] -> memory
+      { vector = (vector memory) // [(writeAddress', x' * y')]
+      , instructionPointer = (instructionPointer memory) + 4
+      }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     x = readParam' Value First
     y = readParam' Value Second
     writeAddress = readParam' Address Third
-    value = x * y
 
-perform (Operation LessThan paramMode) memory = memory 
-  { vector = (vector memory) // [(writeAddress, value)]
-  , instructionPointer = (instructionPointer memory) + 4
-  }
+perform (Operation LessThan paramMode') memory =
+  case resize memory [x, y, writeAddress] of
+    Left resizedMemory -> resizedMemory
+    Right [x', y', writeAddress'] ->
+      let value = if x' < y' then 1 else 0
+      in memory
+        { vector = (vector memory) // [(writeAddress', value)]
+        , instructionPointer = (instructionPointer memory) + 4
+        }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     x = readParam' Value First
     y = readParam' Value Second
     writeAddress = readParam' Address Third
-    value = if x < y then 1 else 0
 
-perform (Operation Equals paramMode) memory = memory 
-  { vector = (vector memory) // [(writeAddress, value)]
-  , instructionPointer = (instructionPointer memory) + 4
-  }
+perform (Operation Equals paramMode') memory =
+  case resize memory [x, y, writeAddress] of
+    Left resizedMemory -> resizedMemory
+    Right [x', y', writeAddress'] ->
+      let value = if x' == y' then 1 else 0
+      in memory
+        { vector = (vector memory) // [(writeAddress', value)]
+        , instructionPointer = (instructionPointer memory) + 4
+        }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     x = readParam' Value First
     y = readParam' Value Second
     writeAddress = readParam' Address Third
-    value = if x == y then 1 else 0
 
-perform (Operation Input paramMode) memory = memory 
-  { vector = (vector memory) // [(writeAddress, x)]
-  , instructionPointer = (instructionPointer memory) + 2
-  , inputs = xs
-  }
+perform (Operation Input paramMode') memory =
+  case resize memory [writeAddress] of
+    Left resizedMemory -> resizedMemory
+    Right [writeAddress'] -> memory
+      { vector = (vector memory) // [(writeAddress', x)]
+      , instructionPointer = (instructionPointer memory) + 2
+      , inputs = xs
+      }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     writeAddress = readParam' Address First
     (x:xs) = inputs memory
 
-perform (Operation Output paramMode) memory = memory 
-  { instructionPointer = (instructionPointer memory) + 2
-  , outputs = x:(outputs memory)
-  }
+perform (Operation Output paramMode') memory =
+  case resize memory [x] of
+    Left resizedMemory -> resizedMemory
+    Right [x'] -> memory
+      { instructionPointer = (instructionPointer memory) + 2
+      , outputs = x':(outputs memory)
+      }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     x = readParam' Value First
 
-perform (Operation JumpTrue paramMode) memory = memory 
-  { instructionPointer = nextPointer }
+perform (Operation JumpTrue paramMode') memory =
+  case resize memory [x, jumpAddress] of
+    Left resizedMemory -> resizedMemory
+    Right [x', jumpAddress'] -> 
+      let nextPointer = if x' /= 0 then jumpAddress' else (instructionPointer memory) + 3
+      in memory { instructionPointer = nextPointer }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     x = readParam' Value First
     jumpAddress = readParam' Value Second
-    nextPointer = if x /= 0 then jumpAddress else (instructionPointer memory) + 3
 
-perform (Operation JumpFalse paramMode) memory = memory 
-  { instructionPointer = nextPointer }
+perform (Operation JumpFalse paramMode') memory =
+  case resize memory [x, jumpAddress] of
+    Left resizedMemory -> resizedMemory
+    Right [x', jumpAddress'] -> 
+      let nextPointer = if x' == 0 then jumpAddress' else (instructionPointer memory) + 3
+      in memory { instructionPointer = nextPointer }
   where
-    readParam' = readParam memory paramMode
+    readParam' = readParam memory paramMode'
     x = readParam' Value First
     jumpAddress = readParam' Value Second
-    nextPointer = if x == 0 then jumpAddress else (instructionPointer memory) + 3
+
+perform (Operation AdjustRelativeBase paramMode') memory =
+  case resize memory [x] of
+    Left resizedMemory -> resizedMemory
+    Right [x'] -> 
+      let newRelativeBase = (relativeBase memory) + x'
+      in memory
+      { instructionPointer = (instructionPointer memory) + 2 
+      , relativeBase = newRelativeBase 
+      }
+  where
+    readParam' = readParam memory paramMode'
+    x = readParam' Value First
 
 perform (Operation Halt _) memory = memory
   { instructionPointer = -1
   , inputs = []
-  , outputs = inputs memory 
   }
 
 processInstruction :: IntCodeMemory -> IntCodeMemory
 processInstruction memory = if continue then processInstruction newMemory else newMemory 
   where
     opcodeInt = (vector memory) ! instructionPointer memory
-    --operation = traceShow (parseOperation opcodeInt) (parseOperation opcodeInt)
     operation = parseOperation opcodeInt
     newMemory = perform operation memory
     continue = case operation of
-                 (Operation Output _) -> False
+                 (Operation Output _) -> True
                  (Operation Halt _) -> False
-                 other -> True
+                 _ -> True
 
 execute :: IntCodeMemory -> IntCodeMemory
 execute memory = processInstruction memory
 
 initMemory :: Vector Int -> IntCodeMemory
-initMemory vector = IntCodeMemory 
+initMemory vec = IntCodeMemory 
   { instructionPointer = 0
   , relativeBase = 0
-  , vector = vector
+  , vector = vec
   , inputs = []
   , outputs = []
   }
