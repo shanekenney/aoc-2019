ddd932ae31d5921bcab414811bae208e81cddad8
Shane Kenney <me@shanek.org>
2019-12-28 12:48:59 +1100

    Attempt day 15 with state monad. Not working

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -9,3 +9,8 @@ notNull = not . null
 manhattenDistance :: (Int, Int) -> (Int, Int) -> Int
 manhattenDistance (x1, y1) (x2, y2) = abs ((x1 - x2) + (y1 - y2))
 
+minMaybe :: Ord a => Maybe a -> Maybe a -> Maybe a
+minMaybe Nothing b = b
+minMaybe a Nothing = a
+minMaybe a b = min a b
+
